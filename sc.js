7ef286565808580f5b021207a6fa6d374b9af979
Sadiq <sadiq78692431@gmail.com>
2025-08-30 20:45:16 +0530

    sms

--- sc.js
+++
@@ -1,7 +0,0 @@
-// Print to console
-console.log("Hello, JavaScript!");
-
-// Variables
-let name = "Rafiq";
-const age = 24;
-console.log("My name is " + name + " and I am " + age + " years old.");
