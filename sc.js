f10bc891a08dc53d0c64bf4ac5ecead449bd32cd
Sadiq <sadiq78692431@gmail.com>
2025-08-30 20:06:10 +0530

    Initial commit

---
+++ sc.js
@@ -0,0 +1,7 @@
+// Print to console
+console.log("Hello, JavaScript!");
+
+// Variables
+let name = "Rafiq";
+const age = 24;
+console.log("My name is " + name + " and I am " + age + " years old.");
